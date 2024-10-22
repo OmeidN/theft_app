cfcd99a5576c63d44f3266cab0ffbd7862feef13
ikligman <ikligman.ames@gmail.com>
2024-10-22 14:23:20 -0700

    search to map

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Search Page'));
+    return Center(child: Text('Map Page'));
   }
 }
