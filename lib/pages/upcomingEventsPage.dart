ab18aff7f83b072c36952a69c87151078ee476e0
Omeid Nadery <omeid.n@icloud.com>
2024-10-24 15:56:48 -0700

    coachella logo

--- lib/pages/upcomingEventsPage.dart
+++ lib/pages/upcomingEventsPage.dart
@@ -15,6 +15,7 @@ class UpcomingEventsPage extends StatelessWidget {
             eventName: 'EDC 2024',
             eventDate: '2024-10-28',
             eventTime: '5:00 PM',
+            logoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRniXJpNK2UFfsnkP5DV1it6CaTzv32lpvSPA&s', // EDC logo
             onPressed: () {
               Navigator.push(
                 context,
@@ -26,6 +27,7 @@ class UpcomingEventsPage extends StatelessWidget {
             eventName: 'Coachella 2024',
             eventDate: '2024-04-12',
             eventTime: '12:00 PM',
+            logoUrl: 'https://play-lh.googleusercontent.com/RHwEuVQUhY-5F6cbF4iMR9rKv2tU7iAaUEk_KLF1ZDgfMc6XsuclC-A81Jz4BxlRXJU=w240-h480-rw', // Coachella logo
             onPressed: () {
               Navigator.push(
                 context,
@@ -43,6 +45,7 @@ class EventButton extends StatelessWidget {
   final String eventName;
   final String eventDate;
   final String eventTime;
+  final String logoUrl; // Add a parameter for the logo URL
   final VoidCallback onPressed;
 
   const EventButton({
@@ -50,6 +53,7 @@ class EventButton extends StatelessWidget {
     required this.eventName,
     required this.eventDate,
     required this.eventTime,
+    required this.logoUrl, // Include logoUrl in constructor
     required this.onPressed,
   }) : super(key: key);
 
@@ -74,10 +78,13 @@ class EventButton extends StatelessWidget {
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(12),
                   child: Image.network(
-                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRniXJpNK2UFfsnkP5DV1it6CaTzv32lpvSPA&s',
+                    logoUrl, // Use the logo URL passed from the parent widget
                     height: 50,
                     width: 50,
                     fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Icon(Icons.error); // Fallback if image fails to load
+                    },
                   ),
                 ),
               ),
