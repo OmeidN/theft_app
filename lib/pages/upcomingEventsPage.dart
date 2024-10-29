701eb1daf697d1cbd99dbaa2665a02075912b2ee
ikligman <ikligman.ames@gmail.com>
2024-10-29 14:22:30 -0700

    Lost lands test

--- lib/pages/upcomingEventsPage.dart
+++ lib/pages/upcomingEventsPage.dart
@@ -17,6 +17,13 @@ class UpcomingEventsPage extends StatelessWidget {
       logoUrl: 'https://play-lh.googleusercontent.com/RHwEuVQUhY-5F6cbF4iMR9rKv2tU7iAaUEk_KLF1ZDgfMc6XsuclC-A81Jz4BxlRXJU=w240-h480-rw',
       mapUrl: 'https://media.coachella.com/content/content_images/452/qsiLKM8QJB256XSDzEgTBmjXM7RqtOExeYwZz6NW.jpg',
     ),
+    Event(
+      name: 'Lost Lands 2023',
+      date: '2024-10-28',
+      time: '5:00 PM',
+      logoUrl: 'https://www.lostlandsfestival.com/wp-content/uploads/2020/02/LL2020Profile.jpg',
+      mapUrl: 'https://preview.redd.it/maps-are-here-v0-em31yzdd6aob1.jpg?width=1080&crop=smart&auto=webp&s=c70648e277732f40824309e465f94c2f900a043b',
+    ),
   ];
 
   @override
